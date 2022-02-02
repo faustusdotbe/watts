3312d6e5a267db34dea77a123af788d284bcdaf3
faustusdotbe <simon.hengchen@gmail.com>
2022-02-02 13:32:06 +0100

    adding python script so i don't need to nano

--- POWERLIMIT.sh
+++ POWERLIMIT.sh
@@ -3,18 +3,18 @@ if [ "$ac_adapter" = "on" ]; then
 	echo $ac_adapter
 	# MSR
 	# PL1
-	echo 16000000 | sudo tee /sys/devices/virtual/powercap/intel-rapl/intel-rapl:0/constraint_0_power_limit_uw # 16 watt
+	echo 12000000 | sudo tee /sys/devices/virtual/powercap/intel-rapl/intel-rapl:0/constraint_0_power_limit_uw # 16 watt
 	echo 28000000 | sudo tee /sys/devices/virtual/powercap/intel-rapl/intel-rapl:0/constraint_0_time_window_us # 28 sec
 	# PL2
-	echo 16000000 | sudo tee /sys/devices/virtual/powercap/intel-rapl/intel-rapl:0/constraint_1_power_limit_uw # 16 watt
+	echo 12000000 | sudo tee /sys/devices/virtual/powercap/intel-rapl/intel-rapl:0/constraint_1_power_limit_uw # 16 watt
 	echo 2440 | sudo tee /sys/devices/virtual/powercap/intel-rapl/intel-rapl:0/constraint_1_time_window_us # 0.00244 sec
 
 	# MCHBAR
 	# PL1
-	echo 16000000 | sudo tee /sys/devices/virtual/powercap/intel-rapl-mmio/intel-rapl-mmio:0/constraint_0_power_limit_uw # 16 watt
+	echo 12000000 | sudo tee /sys/devices/virtual/powercap/intel-rapl-mmio/intel-rapl-mmio:0/constraint_0_power_limit_uw # 16 watt
 	# ^ Only required change on a ASUS Zenbook UX430UNR
 	echo 28000000 | sudo tee /sys/devices/virtual/powercap/intel-rapl-mmio/intel-rapl-mmio:0/constraint_0_time_window_us # 28 sec
 	# PL2
-	echo 16000000 | sudo tee /sys/devices/virtual/powercap/intel-rapl-mmio/intel-rapl-mmio:0/constraint_1_power_limit_uw # 16 watt
+	echo 12000000 | sudo tee /sys/devices/virtual/powercap/intel-rapl-mmio/intel-rapl-mmio:0/constraint_1_power_limit_uw # 16 watt
 	echo 2440 | sudo tee /sys/devices/virtual/powercap/intel-rapl-mmio/intel-rapl-mmio:0/constraint_1_time_window_us # 0.00244 sec
 fi
---
+++ change.py
@@ -0,0 +1,20 @@
+import sys
+import os
+
+new_lim = sys.argv[1]
+
+fout = open("POWERLIMIT.shx", "w")
+with open("POWERLIMIT.sh") as f:
+    for line in f:
+        if "power_limit" in line:
+            split = line.split()
+            split[1] = new_lim + "000000"
+            new_line = "\t" + " ".join(split) + "\n"
+            fout.write(new_line)
+        else:
+            fout.write(line.rstrip()+"\n")
+fout.close()
+
+os.replace("POWERLIMIT.shx","POWERLIMIT.sh")
+
+
